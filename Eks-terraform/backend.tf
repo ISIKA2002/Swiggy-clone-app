54eddd8a02acde0727078874caefcda98fcc1965
ISIKA2002 <113756426+ISIKA2002@users.noreply.github.com>
2024-07-02 12:00:15 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cf-templates-1wh1qcy7rc86n-us-east-1" # Replace with your actual S3 bucket name
+    bucket = "heremybucket" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
